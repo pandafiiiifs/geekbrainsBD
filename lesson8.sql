0e8bcb81d3bdc6293c2a3a7b878d13fd49631abf
pandafiiiifs <66410903+pandafiiiifs@users.noreply.github.com>
2020-09-09 11:16:47 +0300

    Update lesson8.sql

--- lesson8.sql
+++ lesson8.sql
@@ -9,3 +9,15 @@ SELECT  COUNT(profiles.user_id), profiles.gender FROM profiles
  SELECT likes.target_id ,likes.target_type_id ,profiles.birthday 
  FROM profiles 
          INNER JOIN likes ON target_id = profiles.user_id AND target_type_id = 2  ORDER BY birthday LIMIT 10;
+         
+         
+           -- 5. Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной 
+-- сети.     
+
+	 
+	SELECT 
+      CONCAT (users.first_name , ' ' , users.last_name) as user,likes.user_id FROM users 
+       left join likes on likes.user_id = users.id  
+       left join media on media.user_id = users.id
+      left join messages on messages.from_user_id = users.id 
+	  LIMIT 10;
