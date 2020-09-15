e56f4cf2c732a4930a4305ded9a625e492bc35a2
pandafiiiifs <66410903+pandafiiiifs@users.noreply.github.com>
2020-09-15 09:48:14 +0300

    Update lesson8.sql

--- lesson8.sql
+++ lesson8.sql
@@ -6,18 +6,34 @@ SELECT  COUNT(profiles.user_id), profiles.gender FROM profiles
        
         -- 4. Подсчитать количество лайков которые получили 10 самых молодых пользователей. 
 
- SELECT likes.target_id ,likes.target_type_id ,profiles.birthday 
- FROM profiles 
-         INNER JOIN likes ON target_id = profiles.user_id AND target_type_id = 2  ORDER BY birthday LIMIT 10;
+SELECT SUM(got_likes) AS total_likes_for_youngest
+  FROM (   
+    SELECT COUNT(DISTINCT likes.id) AS got_likes 
+      FROM profiles
+        LEFT JOIN likes
+          ON likes.target_id = profiles.user_id
+            AND target_type_id = 2
+      GROUP BY profiles.user_id
+      ORDER BY profiles.birthday DESC
+      LIMIT 10
+) AS youngest;   
          
          
            -- 5. Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной 
 -- сети.     
 
 	 
-	SELECT 
-      CONCAT (users.first_name , ' ' , users.last_name) as user,likes.user_id FROM users 
-       left join likes on likes.user_id = users.id  
-       left join media on media.user_id = users.id
-      left join messages on messages.from_user_id = users.id 
-	  LIMIT 10;
+	SELECT users.id,
+  COUNT(DISTINCT messages.id) + 
+  COUNT(DISTINCT likes.id) + 
+  COUNT(DISTINCT media.id) AS activity 
+  FROM users
+    LEFT JOIN messages 
+      ON users.id = messages.from_user_id
+    LEFT JOIN likes
+      ON users.id = likes.user_id
+    LEFT JOIN media
+      ON users.id = media.user_id
+  GROUP BY users.id
+  ORDER BY activity
+  LIMIT 10;
